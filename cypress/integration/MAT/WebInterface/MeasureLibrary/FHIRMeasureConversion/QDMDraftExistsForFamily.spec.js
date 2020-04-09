b969fe832f28bdac948aaf977e71445ed784260b
NirmalAutomation <nirmalthanga@gmail.com>
2020-04-09 12:54:40 -0400

    MAT-836
    QDM Proportion Measure creation

--- cypress/integration/MAT/WebInterface/MeasureLibrary/FHIRMeasureConversion/QDMDraftExistsForFamily.spec.js
+++ cypress/integration/MAT/WebInterface/MeasureLibrary/FHIRMeasureConversion/QDMDraftExistsForFamily.spec.js
@@ -4,7 +4,7 @@ import * as measureLibrary from "../../../../../pom/MAT/WI/MeasureLibrary";
 
 let measureName = ''
 
-describe('Measure Library: Validate Scenario 3a Conversion to FHIR', () => {
+describe('Measure Library: Validate Scenario 1 Conversion to FHIR', () => {
     before('Login', () => {
         helper.loginGeneric()
 
@@ -17,7 +17,7 @@ describe('Measure Library: Validate Scenario 3a Conversion to FHIR', () => {
         helper.logout()
     })
 
-    it('Scenario 3a: Convert QDM measure to FHIR successfully', () => {
+    it('Scenario 1: QDM/CQL draft exists for that family', () => {
 
         helper.enterText(measureLibrary.searchInputBox, measureName)
         cy.get(measureLibrary.searchBtn).click();
@@ -39,15 +39,29 @@ describe('Measure Library: Validate Scenario 3a Conversion to FHIR', () => {
         helper.visibleWithTimeout(matheader.progressbar)
         helper.notVisibleWithTimeout(matheader.progressbar)
 
-        cy.get(measureLibrary.successfulMessage).should('contain.text', 'has been successfully create');
-
         cy.get(measureLibrary.row1MeasureSearch).click();
-        cy.get(measureLibrary.convertToFhirMeasureSearchBtn).click();
+        cy.get(measureLibrary.createDraftMeasureSearchBtn).click();
 
         helper.visibleWithTimeout(matheader.progressbar)
         helper.notVisibleWithTimeout(matheader.progressbar)
 
-        cy.get(measureLibrary.successfulMessage).should('contain.text', 'has been successfully create');        
+        cy.get('h1').should('contain.text', 'My Measures > Draft Measure');
+
+        cy.get(measureLibrary.saveAndContinueButtonDraft).click();
+
+        cy.get(measureLibrary.confirmationContinue).click();
+
+        cy.get(measureLibrary.measureLibraryTab).click();
+
+        helper.visibleWithTimeout(matheader.progressbar)
+        helper.notVisibleWithTimeout(matheader.progressbar)
+
+        cy.get(measureLibrary.row2MeasureSearch).click();
+        cy.get(measureLibrary.convertToFhirMeasureSearchBtn).click();
+
+        // FHIR Warning Dialog
+        cy.get(measureLibrary.fhirConversionWarningMessage).should('contain.text', 'Only one draft per measure family should be allowed.');
+        cy.get(measureLibrary.fhirConversionReturnBtn).click();
 
     })
 
--- cypress/integration/MAT/WebInterface/MeasureLibrary/FHIRMeasureConversion/SuccessfulConversion.spec.js
+++ cypress/integration/MAT/WebInterface/MeasureLibrary/FHIRMeasureConversion/SuccessfulConversion.spec.js
@@ -4,7 +4,7 @@ import * as measureLibrary from "../../../../../pom/MAT/WI/MeasureLibrary";
 
 let measureName = ''
 
-describe('Measure Library: Validate Scenario 3b Conversion to FHIR', () => {
+describe('Measure Library: Validate Scenario 3a Conversion to FHIR', () => {
     before('Login', () => {
         helper.loginGeneric()
 
@@ -17,7 +17,7 @@ describe('Measure Library: Validate Scenario 3b Conversion to FHIR', () => {
         helper.logout()
     })
 
-    it('Scenario 3b: Reconverting QDM measure: Warning message', () => {
+    it('Scenario 3a: Convert QDM measure to FHIR successfully', () => {
 
         helper.enterText(measureLibrary.searchInputBox, measureName)
         cy.get(measureLibrary.searchBtn).click();
@@ -39,22 +39,11 @@ describe('Measure Library: Validate Scenario 3b Conversion to FHIR', () => {
         helper.visibleWithTimeout(matheader.progressbar)
         helper.notVisibleWithTimeout(matheader.progressbar)
 
-        cy.get(measureLibrary.successfulMessage).should('contain.text', 'has been successfully create');
-
         cy.get(measureLibrary.row1MeasureSearch).click();
         cy.get(measureLibrary.convertToFhirMeasureSearchBtn).click();
 
         helper.visibleWithTimeout(matheader.progressbar)
         helper.notVisibleWithTimeout(matheader.progressbar)
-
-        cy.get(measureLibrary.successfulMessage).should('contain.text', 'has been successfully create');     
-        
-        cy.get(measureLibrary.row2MeasureSearch).click();
-        cy.get(measureLibrary.convertToFhirMeasureSearchBtn).click();
-
-        // FHIR Warning Dialog
-        cy.get(measureLibrary.fhirConversionWarningMessage).should('contain.text', 'Are you sure you want to convert this measure again? The existing FHIR measure will be overwritten.');
-        cy.get(measureLibrary.fhirConversionNoBtn).click();
 
     })
 
