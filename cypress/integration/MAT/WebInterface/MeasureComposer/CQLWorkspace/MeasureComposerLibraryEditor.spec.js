92fbd3818a1aadb33b236d53766bc0d988896105
NirmalAutomation <35673592+NirmalAutomation@users.noreply.github.com>
2020-06-16 15:39:36 -0400

    updates

--- cypress/integration/MAT/WebInterface/MeasureComposer/CQLWorkspace/MeasureComposerLibraryEditor.spec.js
+++ cypress/integration/MAT/WebInterface/MeasureComposer/CQLWorkspace/MeasureComposerLibraryEditor.spec.js
@@ -145,53 +145,53 @@ describe('FHIR Measure: Version error message', () => {
 
 })
 
-// describe('FHIR Measure: Add code directly on CQL Library Editor', () => {
-//     before('Login', () => {
-//         oktaLogin.login()
+describe('FHIR Measure: Add code directly on CQL Library Editor', () => {
+    before('Login', () => {
+        oktaLogin.login()
 
-//         fhirMeasure = helper.createDraftMeasure('fhirDraftMeasure', 'FHIR')
+        fhirMeasure = helper.createDraftMeasure('fhirDraftMeasure', 'FHIR')
 
-//     })
-//     beforeEach('Preserve Cookies', () => {
-//         helper.preserveCookies()
-//     })
-//     after('Log Out', () => {
-//         helper.logout()
-//     })
+    })
+    beforeEach('Preserve Cookies', () => {
+        helper.preserveCookies()
+    })
+    after('Log Out', () => {
+        helper.logout()
+    })
 
-//     it('FHIR Measure: Validate the warning message when editing directly on CQL Library Editor', () => {
+    it('FHIR Measure: Validate the warning message when editing directly on CQL Library Editor', () => {
 
-//         helper.enterText(measurelibrary.searchInputBox, fhirMeasure)
-//         cy.get(measurelibrary.searchBtn).click();
+        helper.enterText(measurelibrary.searchInputBox, fhirMeasure)
+        cy.get(measurelibrary.searchBtn).click();
 
-//         helper.verifySpinnerAppearsAndDissappears()
+        helper.verifySpinnerAppearsAndDissappears()
 
-//         cy.get(measurelibrary.row1MeasureSearch).dblclick();
+        cy.get(measurelibrary.row1MeasureSearch).dblclick();
 
-//         helper.verifySpinnerAppearsAndDissappears()
+        helper.verifySpinnerAppearsAndDissappears()
 
-//         cy.get(measureComposer.cqlWorkspace).click();
+        cy.get(measureComposer.cqlWorkspace).click();
 
-//         helper.verifySpinnerAppearsAndDissappears()
+        helper.verifySpinnerAppearsAndDissappears()
 
-//         //CQL Library Editor
+        //CQL Library Editor
 
-//         cy.get(measureComposer.cqlLibraryEditor).click()
+        cy.get(measureComposer.cqlLibraryEditor).click()
 
-//         cy.get(measureComposer.warningMessage).should('contain.text', 'You are viewing CQL with no validation errors.');
+        cy.get(measureComposer.warningMessage).should('contain.text', 'You are viewing CQL with no validation errors.');
 
-//         helper.verifySpinnerAppearsAndDissappears()
+        helper.verifySpinnerAppearsAndDissappears()
 
-//         cy.get(measureComposer.cqlLibraryEditorInput).type("{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}valueset \"Annual Wellness Visit\": 'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.526.3.1240'")
+        cy.get(measureComposer.cqlLibraryEditorInput).type("{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}valueset \"Annual Wellness Visit\": 'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.526.3.1240'")
 
-//         cy.get(measureComposer.cqlEditorSaveBtn).click()
+        cy.get(measureComposer.cqlEditorSaveBtn).click()
 
-//         cy.get(measureComposer.warningMessage).should('contain.text', 'Changes made to the CQL library declaration and model declaration can not be saved through the CQL Library Editor. Please make those changes in the appropriate areas of the CQL Workspace.')
+        cy.get(measureComposer.warningMessage).should('contain.text', 'Changes made to the CQL library declaration and model declaration can not be saved through the CQL Library Editor. Please make those changes in the appropriate areas of the CQL Workspace.')
 
-//         cy.get(measurelibrary.cqlLibraryTab).click()
+        cy.get(measurelibrary.cqlLibraryTab).click()
 
-//         helper.verifySpinnerAppearsAndDissappears()
+        helper.verifySpinnerAppearsAndDissappears()
 
-//     })
+    })
 
-// })
+})
