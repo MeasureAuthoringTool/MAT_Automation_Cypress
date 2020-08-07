8e0b8da7edda9eb88a52e0b85614b87ee94663c4
NirmalAutomation <nirmalthanga@gmail.com>
2020-08-07 13:32:44 -0400

    MAT-1330

--- cypress/integration/MAT/WebInterface/MeasureComposer/MeasurePackager/ErrorMessageMeasurePackaging.spec.js
+++ cypress/integration/MAT/WebInterface/MeasureComposer/MeasurePackager/ErrorMessageMeasurePackaging.spec.js
@@ -3,6 +3,7 @@ import * as measurelibrary from "../../../../../pom/MAT/WI/MeasureLibrary";
 import * as measureComposer from "../../../../../pom/MAT/WI/MeasureComposer";
 import * as oktaLogin from "../../../../../support/oktaLogin";
 import * as dataCreation from "../../../../../support/MAT/MeasureAndCQLLibraryCreation";
+import * as measureDetails from '../../../../../pom/MAT/WI/MeasureDetails'
 
 let fhirMeasure = ''
 let qdmMeasure = ''
@@ -61,7 +62,7 @@ describe('Measure Packager: Validate before packaging a FHIR measure', () => {
 
     })
 
-    it('FHIR Measure: Validate error message on measure packager page', () => {
+    it('FHIR Measure: Validate error message for cql workspace requirement', () => {
 
         helper.enterText(measurelibrary.searchInputBox, fhirMeasure)
         cy.get(measurelibrary.searchBtn).click();
@@ -71,6 +72,23 @@ describe('Measure Packager: Validate before packaging a FHIR measure', () => {
         cy.get(measurelibrary.row1MeasureSearch).dblclick();
 
         helper.verifySpinnerAppearsAndDissappears()
+
+        //entering required meta data
+        cy.get(measureDetails.measureStewardDeveloper).click()
+        cy.get(measureDetails.measureStewardListBox).select('SemanticBits')
+        cy.get(measureDetails.row1CheckBox).click()
+        cy.get(measureDetails.saveBtn).click()
+        helper.visibleWithTimeout(measureDetails.warningMessage)
+
+        cy.get(measureDetails.description).click()
+        helper.enterText(measureDetails.textAreaInput, 'description')
+        cy.get(measureDetails.saveBtn).click()
+        helper.visibleWithTimeout(measureDetails.warningMessage)
+
+        cy.get(measureDetails.measureType).click()
+        cy.get(measureDetails.row1CheckBox).click()
+        cy.get(measureDetails.saveBtn).click()
+        helper.visibleWithTimeout(measureDetails.warningMessage)
 
         cy.get(measureComposer.cqlWorkspace).click();
 
@@ -90,6 +108,150 @@ describe('Measure Packager: Validate before packaging a FHIR measure', () => {
         helper.verifySpinnerAppearsAndDissappears()
         cy.get(measureComposer.packageWarningMessage).should('contain.text', 'Your CQL file contains validation errors. Errors must be corrected before proceeding to measure packaging. Please return to the CQL Workspace to make corrections.');
 
+    })
+
+})
+
+describe('Measure Packager: Validate the error message for details requirement', () => {
+    before('Login', () => {
+        oktaLogin.login()
+
+        fhirMeasure = dataCreation.createDraftMeasure('FhirDraftMeasure', 'FHIR')
+
+        helper.verifySpinnerAppearsAndDissappears()
+
+    })
+    beforeEach('Preserve Cookies', () => {
+        helper.preserveCookies()
+    })
+    after('Log Out', () => {
+        helper.logout()
+    })
+
+    it('FHIR Measure: Validate error message for missing meta data', () => {
+
+        helper.enterText(measurelibrary.searchInputBox, fhirMeasure)
+        cy.get(measurelibrary.searchBtn).click();
+
+        helper.verifySpinnerAppearsAndDissappears()
+
+        cy.get(measurelibrary.row1MeasureSearch).dblclick();
+
+        helper.verifySpinnerAppearsAndDissappears()
+
+        cy.get(measureComposer.cqlWorkspace).click();
+
+        helper.verifySpinnerAppearsAndDissappears()
+
+        helper.waitToContainText(measureComposer.cqlWorkspaceTitleGeneralInformation, 'General Information')
+
+        //Includes
+
+        cy.get(measureComposer.includes).click()
+
+        cy.get(measureComposer.includesListItems).its('length').should('equal', 3)
+
+        cy.get(measureComposer.includesListItems).eq(0).should('contain.text', 'FHIRHelpers')
+        cy.get(measureComposer.includesListItems).eq(1).should('contain.text', 'Global')
+        cy.get(measureComposer.includesListItems).eq(2).should('contain.text', 'SDE')
+
+        cy.get(measureComposer.searchInputBox).type('tjc', { delay: 50 })
+        cy.get(measureComposer.searchBtn).click()
+        cy.get(measureComposer.availableLibrariesRow1checkbox).click()
+        cy.get(measureComposer.libraryAliasInputBox).type('TJC', { delay: 50 })
+        cy.get(measureComposer.saveIncludes).click()
+
+        helper.visibleWithTimeout(measureComposer.warningMessage)
+
+        //Value Sets
+
+        cy.get(measureComposer.valueSets).click()
+
+        helper.verifySpinnerAppearsAndDissappears()
+
+        dataCreation.addValueSet('2.16.840.1.113883.3.666.5.307')
+        dataCreation.addValueSet('2.16.840.1.113762.1.4.1182.118')
+        dataCreation.addValueSet('2.16.840.1.113762.1.4.1111.161')
+
+        // Codes
+
+        cy.get(measureComposer.valueSets).click()
+
+        helper.verifySpinnerAppearsAndDissappears()
+
+        dataCreation.addCode('CODE:/CodeSystem/SNOMEDCT/Version/2020-03/Code/420523002/Info')
+        dataCreation.addCode('CODE:/CodeSystem/SNOMEDCT/Version/2016-03/Code/419099009/Info')
+        dataCreation.addCode('CODE:/CodeSystem/SNOMEDCT/Version/2017-09/Code/371828006/Info')
+
+        // Definition
+
+        cy.get(measureComposer.definition).click()
+
+        helper.verifySpinnerAppearsAndDissappears()
+
+        dataCreation.addDefinition('Initial Population', 'TJC."Encounter with Principal Diagnosis and Age"')
+        dataCreation.addDefinition('Denominator', 'TJC."Ischemic Stroke Encounter"')
+        dataCreation.addDefinition('Numerator', 'true')
+
+        //CQL Library Editor
+
+        cy.get(measureComposer.cqlLibraryEditor).click()
+
+        helper.waitToContainText(measureComposer.cqlWorkspaceTitleCQLLibraryEditor, 'CQL Library Editor')
+
+        helper.visibleWithTimeout(measureComposer.warningMessage)
+        helper.waitToContainText(measureComposer.warningMessage, 'You are viewing CQL with no validation errors.')
+
+        cy.wait(2000)
+
+        // Population Workspace
+
+        cy.get(measureComposer.populationWorkspace).click()
+
+        helper.verifySpinnerAppearsAndDissappears()
+
+        // Initial Population
+        cy.get(measureComposer.initialPopulation).click()
+
+        helper.verifySpinnerAppearsAndDissappears()
+
+        cy.get(measureComposer.initialPopulationDefinitionListBox).select('Initial Population')
+        cy.get(measureComposer.initialPopulationSaveBtn).click()
+
+        helper.visibleWithTimeout(measureComposer.warningMessage)
+        helper.waitToContainText(measureComposer.warningMessage, 'Changes to Initial Populations have been successfully saved.')
+
+        // Denominator
+        cy.get(measureComposer.denominator).click()
+
+        helper.verifySpinnerAppearsAndDissappears()
+
+        cy.get(measureComposer.denominatorDefinitionListBox).select('Denominator')
+        cy.get(measureComposer.denominatorSaveBtn).click()
+
+        helper.visibleWithTimeout(measureComposer.warningMessage)
+        helper.waitToContainText(measureComposer.warningMessage, 'Changes to Denominators have been successfully saved.')
+
+        // Numerator
+        cy.get(measureComposer.numerator).click()
+
+        helper.verifySpinnerAppearsAndDissappears()
+
+        cy.get(measureComposer.numeratorDefinitionListBox).select('Numerator')
+        cy.get(measureComposer.numeratorSaveBtn).click()
+
+        helper.visibleWithTimeout(measureComposer.warningMessage)
+        helper.waitToContainText(measureComposer.warningMessage, 'Changes to Numerators have been successfully saved.')
+
+        // Measure Packager 
+        cy.get(measureComposer.measurePackager).click();
+        helper.verifySpinnerAppearsAndDissappears()
+        cy.get(measureComposer.packageWarningMessage).should('contain.text', ' Please enter the Measure Description prior to packaging.');
+
+        cy.get(measurelibrary.measureLibraryTab).click()
+
+        helper.verifySpinnerAppearsAndDissappears()
+        
     })
 
 })
