e998a37fd46d3253fd94367d13458df077b6082d
ben.seiker <ben.seiker@semanticbits.com>
2020-08-13 13:54:24 -0400

    committing completed part 1 of 1558 testing initial population message of fhir

---
+++ cypress/integration/Bonnie/WebInterface/Dashboard/CreatePatient.spec.js
@@ -0,0 +1,32 @@
+import * as helper from '../../../../support/helpers'
+import * as bonnieLogin from '../../../../support/Bonnie/BonnieLoginLogout'
+import * as CreatePatient from '../../../../pom/Bonnie/WI/CreatePatient'
+import { clickForce } from '../../../../support/helpers'
+
+describe('Create Patient From Dashboard', () => {
+  before('Login', () => {
+
+    bonnieLogin.login()
+
+  })
+  after('Log Out', () => {
+
+    //bonnieLogin.logout()
+
+  })
+
+  it('Create Patient For Top Level Maesure', () => {
+    //helper.enabledWithTimeout(CreatePatient.createNewPatientButton)
+    cy.get(CreatePatient.createNewPatientButton).contains("add new patient to measure").click({ force: true })
+
+    helper.enabledWithTimeout(CreatePatient.patientLastNameInputField)
+    let time = new Date()
+    let append = time.getMonth() + '_' + time.getDay() + '_' + time.getFullYear() + '_' + time.getHours() + '_' + time.getMinutes() + '_' + time.getSeconds()
+    helper.enterText(CreatePatient.patientLastNameInputField, "TestLastName" + append)
+    helper.enterText(CreatePatient.patientFirstNameInputField, "TestFirstName" + append)
+    helper.enterText(CreatePatient.patientBirthDateField, "01/01/1981")
+    helper.enterText(CreatePatient.patientDescriptionNotesField, "This is Test Patient " + append)
+
+
+  })
+})
