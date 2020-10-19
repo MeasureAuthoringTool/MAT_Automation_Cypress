0d3648522c60e9ad180c743688e12a3a977c97e2
cvasile001 <chris.vasile@semanticbits.com>
2020-10-19 16:23:25 -0400

    removing comments

--- cypress/integration/MAT/WebInterface/CQLComposer/CQLLibraryWorkspace/Parameter.spec.js
+++ cypress/integration/MAT/WebInterface/CQLComposer/CQLLibraryWorkspace/Parameter.spec.js
@@ -27,43 +27,43 @@ describe('CQL Composer: CQL Library Workspace: Parameter', () => {
     after('Log Out', () => {
         helper.logout()
     })
-    // it('QDM: Verify errors are coming from correct source', () => {
-    //     helper.verifySpinnerAppearsAndDissappears()
-    //
-    //     helper.enabledWithTimeout(cqlLibrary.searchInputBox)
-    //     helper.enterText(cqlLibrary.searchInputBox, qdmcqlLibrary)
-    //     cy.get(cqlLibrary.searchBtn).click()
-    //
-    //     helper.verifySpinnerAppearsAndDissappears()
-    //
-    //     cy.get(cqlLibrary.row1CqlLibrarySearch).dblclick()
-    //
-    //     helper.verifySpinnerAppearsAndDissappears()
-    //
-    //     cy.get(cqlComposer.parameter).click()
-    //
-    //     helper.waitToContainText(cqlComposer.cqlWorkspaceTitleGlobal2,'Parameter')
-    //
-    //     cy.get(cqlComposer.parameterListbox).select('Measurement Period')
-    //     cy.get(cqlComposer.parameterListbox).contains('Measurement Period').dblclick()
-    //
-    //     cy.get(cqlComposer.parameterEraseBtn).click()
-    //     cy.get(cqlComposer.parameterCQLExpressionEditorInput).type('sdffgsdffgsdfg', { delay: 50 })
-    //     cy.get(cqlComposer.parameterSaveBtn).click()
-    //
-    //     helper.verifySpinnerAppearsAndDissappears()
-    //
-    //     helper.visibleWithTimeout(cqlComposer.warningMessage)
-    //
-    //     //This error is specific to QDM and confirms we are getting errors from QDM source
-    //     cy.get(cqlComposer.editorLeftPanel).click()
-    //     cy.get(cqlComposer.editorErrorToolTip).should('contain.text','ERROR:A named type is required in this context.ERROR:class org.hl7.elm.r1.Null cannot be cast to class org.hl7.elm.r1.TypeSpecifier (org.hl7.elm.r1.Null and org.hl7.elm.r1.TypeSpecifier are in unnamed module of loader org.apache.catalina.loader.ParallelWebappClassLoader' )
-    //
-    //     cy.get(measurelibrary.cqlLibraryTab).click()
-    //
-    //     helper.verifySpinnerAppearsAndDissappears()
-    //
-    // })
+    it('QDM: Verify errors are coming from correct source', () => {
+        helper.verifySpinnerAppearsAndDissappears()
+
+        helper.enabledWithTimeout(cqlLibrary.searchInputBox)
+        helper.enterText(cqlLibrary.searchInputBox, qdmcqlLibrary)
+        cy.get(cqlLibrary.searchBtn).click()
+
+        helper.verifySpinnerAppearsAndDissappears()
+
+        cy.get(cqlLibrary.row1CqlLibrarySearch).dblclick()
+
+        helper.verifySpinnerAppearsAndDissappears()
+
+        cy.get(cqlComposer.parameter).click()
+
+        helper.waitToContainText(cqlComposer.cqlWorkspaceTitleGlobal2,'Parameter')
+
+        cy.get(cqlComposer.parameterListbox).select('Measurement Period')
+        cy.get(cqlComposer.parameterListbox).contains('Measurement Period').dblclick()
+
+        cy.get(cqlComposer.parameterEraseBtn).click()
+        cy.get(cqlComposer.parameterCQLExpressionEditorInput).type('sdffgsdffgsdfg', { delay: 50 })
+        cy.get(cqlComposer.parameterSaveBtn).click()
+
+        helper.verifySpinnerAppearsAndDissappears()
+
+        helper.visibleWithTimeout(cqlComposer.warningMessage)
+
+        //This error is specific to QDM and confirms we are getting errors from QDM source
+        cy.get(cqlComposer.editorLeftPanel).click()
+        cy.get(cqlComposer.editorErrorToolTip).should('contain.text','ERROR:A named type is required in this context.ERROR:class org.hl7.elm.r1.Null cannot be cast to class org.hl7.elm.r1.TypeSpecifier (org.hl7.elm.r1.Null and org.hl7.elm.r1.TypeSpecifier are in unnamed module of loader org.apache.catalina.loader.ParallelWebappClassLoader' )
+
+        cy.get(measurelibrary.cqlLibraryTab).click()
+
+        helper.verifySpinnerAppearsAndDissappears()
+
+    })
     it('FHIR: Verify errors are coming from correct source', () => {
         helper.verifySpinnerAppearsAndDissappears()
 
