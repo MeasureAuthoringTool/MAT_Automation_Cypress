546ecdd01cf99bb2f69b567eeff49aff2d5987e5
NirmalAutomation <nirmalthanga@gmail.com>
2020-11-03 15:57:17 -0500

    Updating Delete Patient and Measure

--- cypress/integration/BonnieFHIR/WebInterface/AttributeUI/RatioWidget.spec.js
+++ cypress/integration/BonnieFHIR/WebInterface/AttributeUI/RatioWidget.spec.js
@@ -2,6 +2,8 @@ import * as helper from '../../../../support/helpers'
 import * as bonnieLogin from '../../../../support/BonnieFHIR/BonnieLoginLogout'
 import * as homePage from '../../../../pom/BonnieFHIR/WI/Homepage'
 import * as measureDetailsPage from '../../../../pom/BonnieFHIR/WI/MeasureDetailsPage'
+import * as deletePatient from '../../../../support/BonnieFHIR/DeletePatient'
+import * as deleteMeasure from '../../../../support/BonnieFHIR/DeleteMeasure'
 import * as testPatientPage from '../../../../pom/BonnieFHIR/WI/TestPatientPage'
 import * as bonnieUploadMeasure from '../../../../support/BonnieFHIR/BonnieUploadMeasure'
 
@@ -37,13 +39,13 @@ describe('Attribute UI: Ratio Widget', () => {
       verifyPatientAdded(initialPatientCount, distinctLastName)
       measureDetailsPage.navigateToHomeMeasurePage()
       navigateToMeasureDetails(measureName)
-      deletePatient(distinctLastName)
+      deletePatient.DeletePatient(distinctLastName)
       verifyPatientRemoved(initialPatientCount)
     })
 
     helper.visibleWithTimeout(measureDetailsPage.measurePageNavigationBtn)
 
-    deleteMeasure(measureName)
+    deleteMeasure.DeleteMeasure(measureName)
 
     helper.visibleWithTimeout(measureDetailsPage.measurePageNavigationBtn)
   })
@@ -58,15 +60,6 @@ describe('Attribute UI: Ratio Widget', () => {
     // cy.wait(1000)
     cy.get(measureDetailsPage.measureDetailsTitle).should('contain.text', 'Measure details')
     cy.log('navigateToMeasureDetails - done')
-  }
-
-  function deletePatient (lastName) {
-    cy.log('deletePatient')
-    const patient = getPatientRecord(lastName)
-    patient.find(measureDetailsPage.patientExpandBtn).click()
-    cy.get(measureDetailsPage.patientInverseBtn).click()
-    cy.get(measureDetailsPage.patientDeleteBtn).click()
-    cy.log('deletePatient - done')
   }
 
   function enterPatientCharacteristics (lastName) {
@@ -100,7 +93,7 @@ describe('Attribute UI: Ratio Widget', () => {
     cy.get(testPatientPage.attributeTypeSelect).select('Ratio')
     cy.get(testPatientPage.ratioValueField).eq(0).type(13)
     cy.get(testPatientPage.ratioValueField).eq(1).type(25)
-    cy.get(testPatientPage.addWidgetBtn).click()
+    cy.get(testPatientPage.addWidgetBtn).eq(0).click()
     cy.get(testPatientPage.exsistingAttribute).contains("dosageInstruction.doseAndRate.rate: 13 '' : 25 ''")
     cy.log('AddRatioWidget - done')
   }
@@ -122,15 +115,6 @@ describe('Attribute UI: Ratio Widget', () => {
     cy.get(measureDetailsPage.newStatus).should('have.text', 'NEW')
     cy.get(measureDetailsPage.patientListing).should('have.text', (initialPatientCount).toString())
     cy.log('verifyPatientRemoved - done')
-  }
-
-  function deleteMeasure (measureName) {
-    cy.log('deleteMeasure')
-    measureDetailsPage.navigateToHomeMeasurePage()
-    navigateToMeasureDetails(measureName)
-
-    measureDetailsPage.clickDeleteMeasure()
-    cy.log('deleteMeasure - done')
   }
 
 })
