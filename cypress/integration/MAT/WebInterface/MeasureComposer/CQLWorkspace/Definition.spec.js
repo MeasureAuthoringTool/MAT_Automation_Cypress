9be0f9457eb3dc71fba4f7859cf3d7d05123f59c
cvasile001 <chris.vasile@semanticbits.com>
2020-12-13 12:43:41 -0500

    Test Case updates

--- cypress/integration/MAT/WebInterface/MeasureComposer/CQLWorkspace/Definition.spec.js
+++ cypress/integration/MAT/WebInterface/MeasureComposer/CQLWorkspace/Definition.spec.js
@@ -22,136 +22,136 @@ describe('Measure Composer: CQL Workspace: Definition', () => {
     after('Log Out', () => {
         helper.logout()
     })
-    // it('Enabled/Disabled QDM Measure Owner', () => {
-    //
-    //     helper.enterText(measurelibrary.searchInputBox, qdmMeasure)
-    //     cy.get(measurelibrary.searchBtn).click()
-    //
-    //     helper.verifySpinnerAppearsAndDissappears()
-    //
-    //     gridRowActions.doubleClickRow(measurelibrary.row1MeasureSearch)
-    //
-    //     helper.verifySpinnerAppearsAndDissappears()
-    //
-    //     cy.get(measureComposer.cqlWorkspace).click()
-    //
-    //     helper.verifySpinnerAppearsAndDissappears()
-    //
-    //     cy.get(measureComposer.definition).click()
-    //
-    //     helper.waitToContainText(measureComposer.cqlWorkspaceTitleGlobal2,'Definition')
-    //
-    //     helper.enabled(measureComposer.addNewBtn)
-    //     helper.enabled(measureComposer.definitionInformationBtn)
-    //     helper.enabled(measureComposer.definitionInsertBtn)
-    //     helper.enabled(measureComposer.definitionExpressionBuilderBtn)
-    //     helper.enabled(measureComposer.definitionSaveBtn)
-    //     helper.enabled(measureComposer.definitionEraseBtn)
-    //     helper.disabled(measureComposer.definitionDeleteBtn)
-    //
-    //     cy.get(measurelibrary.measureLibraryTab).click()
-    //
-    //     helper.verifySpinnerAppearsAndDissappears()
-    //
-    // })
-    // it('Enabled/Disabled FHIR Measure Owner', () => {
-    //
-    //     helper.enterText(measurelibrary.searchInputBox, fhirMeasure)
-    //     cy.get(measurelibrary.searchBtn).click()
-    //
-    //     helper.verifySpinnerAppearsAndDissappears()
-    //
-    //     gridRowActions.doubleClickRow(measurelibrary.row1MeasureSearch)
-    //
-    //     helper.verifySpinnerAppearsAndDissappears()
-    //
-    //     cy.get(measureComposer.cqlWorkspace).click()
-    //
-    //     helper.verifySpinnerAppearsAndDissappears()
-    //
-    //     cy.get(measureComposer.definition).click()
-    //
-    //     helper.waitToContainText(measureComposer.cqlWorkspaceTitleGlobal2,'Definition')
-    //
-    //     helper.enabled(measureComposer.addNewBtn)
-    //     helper.enabled(measureComposer.definitionInformationBtn)
-    //     helper.enabled(measureComposer.definitionInsertBtn)
-    //     helper.disabled(measureComposer.definitionExpressionBuilderBtn)
-    //     helper.enabled(measureComposer.definitionSaveBtn)
-    //     helper.enabled(measureComposer.definitionEraseBtn)
-    //     helper.disabled(measureComposer.definitionDeleteBtn)
-    //
-    //     cy.get(measurelibrary.measureLibraryTab).click()
-    //
-    //     helper.verifySpinnerAppearsAndDissappears()
-    //
-    // })
-    // it('QDM Insert Attribute Data population', () => {
-    //
-    //     helper.enterText(measurelibrary.searchInputBox, qdmMeasure)
-    //     cy.get(measurelibrary.searchBtn).click()
-    //
-    //     helper.verifySpinnerAppearsAndDissappears()
-    //
-    //     gridRowActions.doubleClickRow(measurelibrary.row1MeasureSearch)
-    //
-    //     helper.verifySpinnerAppearsAndDissappears()
-    //
-    //     cy.get(measureComposer.cqlWorkspace).click()
-    //
-    //     helper.verifySpinnerAppearsAndDissappears()
-    //
-    //     cy.get(measureComposer.definition).click()
-    //
-    //     helper.waitToContainText(measureComposer.cqlWorkspaceTitleGlobal2,'Definition')
-    //
-    //     cy.get(measureComposer.definitionInsertBtn).click()
-    //     cy.get(measureComposer.itemTypeListBox).select('Attributes')
-    //
-    //     //verifying a unique datatype and attribute based on model type QDM, this ensures the correct data has been populated
-    //     cy.get(measureComposer.attributesListBox).select('activeDatetime').should('have.value', 'activeDatetime')
-    //     cy.get(measureComposer.attributesDataTypeListBox).select('Assessment, Not Ordered').should('have.value', 'Assessment, Not Ordered')
-    //
-    //     cy.get(measureComposer.cancelBtn).click()
-    //
-    //     cy.get(measurelibrary.measureLibraryTab).click()
-    //
-    //     helper.verifySpinnerAppearsAndDissappears()
-    //
-    // })
-    // it('FHIR Insert Attribute Data population', () => {
-    //
-    //     helper.enterText(measurelibrary.searchInputBox, fhirMeasure)
-    //     cy.get(measurelibrary.searchBtn).click()
-    //
-    //     helper.verifySpinnerAppearsAndDissappears()
-    //
-    //     gridRowActions.doubleClickRow(measurelibrary.row1MeasureSearch)
-    //
-    //     helper.verifySpinnerAppearsAndDissappears()
-    //
-    //     cy.get(measureComposer.cqlWorkspace).click()
-    //
-    //     helper.verifySpinnerAppearsAndDissappears()
-    //
-    //     cy.get(measureComposer.definition).click()
-    //
-    //     helper.waitToContainText(measureComposer.cqlWorkspaceTitleGlobal2,'Definition')
-    //
-    //     cy.get(measureComposer.definitionInsertBtn).click()
-    //     cy.get(measureComposer.itemTypeListBox).select('Attributes')
-    //
-    //     //verifying a unique datatype and attribute based on model type FHIR, this ensures the correct data has been populated
-    //     cy.get(measureComposer.attributesDataTypeListBox).select('Condition').should('have.value', 'Condition')
-    //     cy.get(measureComposer.attributesListBox).select('recorder').should('have.value', 'recorder')
-    //
-    //     cy.get(measureComposer.cancelBtn).click()
-    //
-    //     cy.get(measurelibrary.measureLibraryTab).click()
-    //
-    //     helper.verifySpinnerAppearsAndDissappears()
-    //
-    // })
+    it('Enabled/Disabled QDM Measure Owner', () => {
+
+        helper.enterText(measurelibrary.searchInputBox, qdmMeasure)
+        cy.get(measurelibrary.searchBtn).click()
+
+        helper.verifySpinnerAppearsAndDissappears()
+
+        gridRowActions.doubleClickRow(measurelibrary.row1MeasureSearch)
+
+        helper.verifySpinnerAppearsAndDissappears()
+
+        cy.get(measureComposer.cqlWorkspace).click()
+
+        helper.verifySpinnerAppearsAndDissappears()
+
+        cy.get(measureComposer.definition).click()
+
+        helper.waitToContainText(measureComposer.cqlWorkspaceTitleGlobal2,'Definition')
+
+        helper.enabled(measureComposer.addNewBtn)
+        helper.enabled(measureComposer.definitionInformationBtn)
+        helper.enabled(measureComposer.definitionInsertBtn)
+        helper.enabled(measureComposer.definitionExpressionBuilderBtn)
+        helper.enabled(measureComposer.definitionSaveBtn)
+        helper.enabled(measureComposer.definitionEraseBtn)
+        helper.disabled(measureComposer.definitionDeleteBtn)
+
+        cy.get(measurelibrary.measureLibraryTab).click()
+
+        helper.verifySpinnerAppearsAndDissappears()
+
+    })
+    it('Enabled/Disabled FHIR Measure Owner', () => {
+
+        helper.enterText(measurelibrary.searchInputBox, fhirMeasure)
+        cy.get(measurelibrary.searchBtn).click()
+
+        helper.verifySpinnerAppearsAndDissappears()
+
+        gridRowActions.doubleClickRow(measurelibrary.row1MeasureSearch)
+
+        helper.verifySpinnerAppearsAndDissappears()
+
+        cy.get(measureComposer.cqlWorkspace).click()
+
+        helper.verifySpinnerAppearsAndDissappears()
+
+        cy.get(measureComposer.definition).click()
+
+        helper.waitToContainText(measureComposer.cqlWorkspaceTitleGlobal2,'Definition')
+
+        helper.enabled(measureComposer.addNewBtn)
+        helper.enabled(measureComposer.definitionInformationBtn)
+        helper.enabled(measureComposer.definitionInsertBtn)
+        helper.disabled(measureComposer.definitionExpressionBuilderBtn)
+        helper.enabled(measureComposer.definitionSaveBtn)
+        helper.enabled(measureComposer.definitionEraseBtn)
+        helper.disabled(measureComposer.definitionDeleteBtn)
+
+        cy.get(measurelibrary.measureLibraryTab).click()
+
+        helper.verifySpinnerAppearsAndDissappears()
+
+    })
+    it('QDM Insert Attribute Data population', () => {
+
+        helper.enterText(measurelibrary.searchInputBox, qdmMeasure)
+        cy.get(measurelibrary.searchBtn).click()
+
+        helper.verifySpinnerAppearsAndDissappears()
+
+        gridRowActions.doubleClickRow(measurelibrary.row1MeasureSearch)
+
+        helper.verifySpinnerAppearsAndDissappears()
+
+        cy.get(measureComposer.cqlWorkspace).click()
+
+        helper.verifySpinnerAppearsAndDissappears()
+
+        cy.get(measureComposer.definition).click()
+
+        helper.waitToContainText(measureComposer.cqlWorkspaceTitleGlobal2,'Definition')
+
+        cy.get(measureComposer.definitionInsertBtn).click()
+        cy.get(measureComposer.itemTypeListBox).select('Attributes')
+
+        //verifying a unique datatype and attribute based on model type QDM, this ensures the correct data has been populated
+        cy.get(measureComposer.attributesListBox).select('activeDatetime').should('have.value', 'activeDatetime')
+        cy.get(measureComposer.attributesDataTypeListBox).select('Assessment, Not Ordered').should('have.value', 'Assessment, Not Ordered')
+
+        cy.get(measureComposer.cancelBtn).click()
+
+        cy.get(measurelibrary.measureLibraryTab).click()
+
+        helper.verifySpinnerAppearsAndDissappears()
+
+    })
+    it('FHIR Insert Attribute Data population', () => {
+
+        helper.enterText(measurelibrary.searchInputBox, fhirMeasure)
+        cy.get(measurelibrary.searchBtn).click()
+
+        helper.verifySpinnerAppearsAndDissappears()
+
+        gridRowActions.doubleClickRow(measurelibrary.row1MeasureSearch)
+
+        helper.verifySpinnerAppearsAndDissappears()
+
+        cy.get(measureComposer.cqlWorkspace).click()
+
+        helper.verifySpinnerAppearsAndDissappears()
+
+        cy.get(measureComposer.definition).click()
+
+        helper.waitToContainText(measureComposer.cqlWorkspaceTitleGlobal2,'Definition')
+
+        cy.get(measureComposer.definitionInsertBtn).click()
+        cy.get(measureComposer.itemTypeListBox).select('Attributes')
+
+        //verifying a unique datatype and attribute based on model type FHIR, this ensures the correct data has been populated
+        cy.get(measureComposer.attributesDataTypeListBox).select('Condition').should('have.value', 'Condition')
+        cy.get(measureComposer.attributesListBox).select('recorder').should('have.value', 'recorder')
+
+        cy.get(measureComposer.cancelBtn).click()
+
+        cy.get(measurelibrary.measureLibraryTab).click()
+
+        helper.verifySpinnerAppearsAndDissappears()
+
+    })
 
     
     it('FHIR Measure: Validate the Attribute drop down is disabled', () => {
