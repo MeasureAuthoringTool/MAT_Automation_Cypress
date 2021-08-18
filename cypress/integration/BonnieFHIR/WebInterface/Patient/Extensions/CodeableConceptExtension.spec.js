ab2e13fe14c36a21693512b274a176e5e130918a
praveena.vasireddy <praveena.vasireddy@semanticbits.com>
2021-08-18 14:56:11 -0400

    Test case to Add Codeable Concept to Extension Choice Type drop down.

---
+++ cypress/integration/BonnieFHIR/WebInterface/Patient/Extensions/CodeableConceptExtension.spec.js
@@ -0,0 +1,85 @@
+import * as helper from '../../../../../support/helpers'
+import * as bonnieLogin from '../../../../../support/Bonnie/BonnieLoginLogout'
+import * as homePage from '../../../../../pom/BonnieFHIR/WI/Homepage'
+import * as measureDetailsPage from '../../../../../pom/BonnieFHIR/WI/MeasureDetailsPage'
+import * as testPatientPage from '../../../../../pom/BonnieFHIR/WI/TestPatientPage'
+import * as bonnieUploadMeasure from '../../../../../support/Bonnie/BonnieFHIR/BonnieUploadMeasure'
+
+describe('Test Patient: Extensions section', () => {
+
+  const measureName = 'FHIRmeasureCMS347'
+  const measureFileToUpload = 'FHIR/FHIRmeasureCMS347-v0-0-003-FHIR-4-0-1.zip'
+
+  beforeEach('Login', () => {
+    bonnieLogin.login()
+  })
+  afterEach('Log Out', () => {
+    bonnieLogin.logout()
+  })
+
+  it.only('Validate the Extensions Components', () => {
+    bonnieUploadMeasure.UploadMeasureToBonnie(measureFileToUpload)
+
+    navigateToMeasureDetails(measureName)
+
+    const lastNameSuffix = new Date().getTime()
+    const distinctLastName = 'President' + lastNameSuffix
+
+    cy.get(measureDetailsPage.patientListing).then((patientListing) => {
+      const initialPatientCount = parseInt(patientListing.text(), 10)
+      cy.log('patient count was:' + initialPatientCount)
+
+      measureDetailsPage.clickAddPatient()
+      enterPatientCharacteristics(distinctLastName)
+
+      testPatientPage.dragAndDrop('diagnostics', 'Diagnostics: Observation: LDL Cholesterol', 23)
+
+      codeableConceptExtension()
+      testPatientPage.clickSavePatient()
+      testPatientPage.verifyPatientAdded(initialPatientCount, distinctLastName)
+      // verifyPatientAdded(initialPatientCount, distinctLastName)
+      measureDetailsPage.navigateToHomeMeasurePage()
+      navigateToMeasureDetails(measureName)
+    })
+
+    helper.visibleWithTimeout(measureDetailsPage.measurePageNavigationBtn)
+
+  })
+
+  function navigateToMeasureDetails (measureName) {
+    cy.log('navigateToMeasureDetails')
+    cy.get(homePage.measure).contains(measureName).click()
+    // cy.wait(1000)
+    cy.get(measureDetailsPage.measureDetailsTitle).should('contain.text', 'Measure details')
+    cy.log('navigateToMeasureDetails - done')
+  }
+
+  function enterPatientCharacteristics (lastName) {
+    cy.log('enterPatientCharacteristics')
+    cy.get(testPatientPage.lastNameTextField).type(lastName)
+    cy.get(testPatientPage.firstNameTextField).type('Current')
+    cy.get(testPatientPage.patientDescriptionTextField).type('Patient is very special')
+    cy.get(testPatientPage.dateofBithField).type('01/01/1950')
+    cy.get(testPatientPage.patientDescriptionTextField).click()
+    cy.get(testPatientPage.raceDropdown).select('Asian')
+    cy.get(testPatientPage.genderDropdown).select('Male')
+    cy.get(testPatientPage.ethnicityDropdown).select('Not Hispanic or Latino')
+    cy.log('enterPatientCharacteristics - done')
+  }
+
+  function codeableConceptExtension () {
+    cy.log('validateCodeableConceptExtension')
+    cy.get(testPatientPage.extensionsShow).click()
+    cy.get(testPatientPage.extensionsUrlField).type('https://google.com')
+    cy.get(testPatientPage.extensionsValueDropDown).select('CodeableConcept')
+    cy.get(testPatientPage.valueSetDirectRefSelect).select('active')
+    cy.get(testPatientPage.extensionAddWidgetBtn).eq(0).click()
+    cy.get(testPatientPage.exsistingExtension).contains('https://google.com')
+    cy.get(testPatientPage.exsistingExtensionUrl).click()
+      .then(() => {
+        cy.get(testPatientPage.exsistingExtension).contains('[ConditionClinicalStatusCodes: active]')
+      })
+    cy.log('CodeableConceptExtensionValidation - done')
+  }
+
+})
