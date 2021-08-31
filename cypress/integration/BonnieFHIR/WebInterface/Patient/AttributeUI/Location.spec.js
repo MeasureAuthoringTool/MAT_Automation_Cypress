a5b6b312892bf872c76fd29b4cdf643477cb1a9e
praveena.vasireddy <praveena.vasireddy@semanticbits.com>
2021-08-31 12:49:24 -0400

    Updated test case with save patient for Location attribute.

--- cypress/integration/BonnieFHIR/WebInterface/Patient/AttributeUI/Location.spec.js
+++ cypress/integration/BonnieFHIR/WebInterface/Patient/AttributeUI/Location.spec.js
@@ -1,39 +1,75 @@
 import * as bonnieLogin from '../../../../../support/Bonnie/BonnieLoginLogout'
 import * as bonnieUploadMeasure from '../../../../../support/Bonnie/BonnieUploadMeasure'
-import * as dashboard from '../../../../../pom/BonnieFHIR/WI/Dashboard'
 import * as measureDetailsPage from '../../../../../pom/BonnieFHIR/WI/MeasureDetailsPage'
 import * as testPatientPage from '../../../../../pom/BonnieFHIR/WI/TestPatientPage'
 import * as helper from '../../../../../support/helpers'
+import * as deletePatient from '../../../../../support/Bonnie/BonnieFHIR/DeletePatient'
+import * as homePage from '../../../../../pom/BonnieFHIR/WI/Homepage'
 
-const lastNameSuffix = new Date().getTime()
-const distinctLastName = "President" + lastNameSuffix
+const measureName = 'SBTESTCMS347'
+const measureFileToUpload = 'FHIR/SBTESTCMS347-v0-0-008-FHIR-4-0-1 (1).zip'
 
-const measureNameWithLocation = "SBTESTCMS347"
-const measureFileNameWithLocation = "FHIR/SBTESTCMS347-v0-0-008-FHIR-4-0-1 (1).zip"
-
-describe("Measure with Loaction", () => {
-  beforeEach("Login", () => {
+describe('Measure with Loaction', () => {
+  beforeEach('Login', () => {
     bonnieLogin.login()
   })
 
-  afterEach("Log Out", () => {
+  afterEach('Log Out', () => {
     bonnieLogin.logout()
   })
 
-  it("can add Location to Encounter", () => {
-    bonnieUploadMeasure.UploadMeasureToBonnie(
-      measureFileNameWithLocation,
-      false
-    )
+  it('can add Location to Encounter', () => {
+    uploadTestMeasure()
 
-    dashboard.navigateToMeasureDetails(measureNameWithLocation)
+    navigateToMeasureDetails(measureName)
 
-    measureDetailsPage.clickAddPatient()
-    testPatientPage.enterPatientCharacteristics(distinctLastName)
+    const lastNameSuffix = new Date().getTime()
+    const distinctLastName = 'President' + lastNameSuffix
 
-    // Drag and drop Encounter from the left data elements list
-    testPatientPage.dragAndDrop('management', 'Management: Encounter: Emergency Department Visit', '36')
+    cy.get(measureDetailsPage.patientListing).then((patientListing) => {
+      const initialPatientCount = parseInt(patientListing.text(), 10)
+      cy.log('patient count was:' + initialPatientCount)
 
+      measureDetailsPage.clickAddPatient()
+      enterPatientCharacteristics(distinctLastName)
+      testPatientPage.dragAndDrop('management', 'Management: Encounter: Emergency Department Visit', 36)
+      addLocation()
+      testPatientPage.clickSavePatient()
+      testPatientPage.verifyPatientAdded(initialPatientCount, distinctLastName)
+      measureDetailsPage.navigateToHomeMeasurePage()
+      navigateToMeasureDetails(measureName)
+      deletePatient.DeletePatient(distinctLastName)
+      deletePatient.VerifyPatientRemoved(initialPatientCount)
+    })
+
+    helper.visibleWithTimeout(measureDetailsPage.measurePageNavigationBtn)
+  })
+
+  function uploadTestMeasure () {
+    bonnieUploadMeasure.UploadMeasureToBonnie(measureFileToUpload)
+  }
+
+  function navigateToMeasureDetails (measureName) {
+    cy.log('navigateToMeasureDetails')
+    cy.get(homePage.measure).contains(measureName).click()
+    cy.get(measureDetailsPage.measureDetailsTitle).should('contain.text', 'Measure details')
+    cy.log('navigateToMeasureDetails - done')
+  }
+
+  function enterPatientCharacteristics (lastName) {
+    cy.log('enterPatientCharacteristics')
+    cy.get(testPatientPage.lastNameTextField).type(lastName)
+    cy.get(testPatientPage.firstNameTextField).type('Current')
+    cy.get(testPatientPage.patientDescriptionTextField).type('Patient is very special')
+    cy.get(testPatientPage.dateofBithField).type('01/01/1950')
+    cy.get(testPatientPage.patientDescriptionTextField).click()
+    cy.get(testPatientPage.raceDropdown).select('Asian')
+    cy.get(testPatientPage.genderDropdown).select('Male')
+    cy.get(testPatientPage.ethnicityDropdown).select('Not Hispanic or Latino')
+    cy.log('enterPatientCharacteristics - done')
+  }
+
+  function addLocation () {
     // Add location Attribute with Location
     cy.log('location.Location')
     cy.get(testPatientPage.attributeNameSelect).select('location')
@@ -70,7 +106,7 @@ describe("Measure with Loaction", () => {
     // Verify the Location's data element is added
     cy.get('.patient-criteria form').should('contain.text', 'location: period')
 
-   //Add location attribute with Location and Period
+    // Add location attribute with Location and Period
     cy.log('location.Location')
     cy.get(testPatientPage.attributeNameSelect).select('location')
     cy.get(testPatientPage.locationSelect).select('Location')
@@ -89,11 +125,5 @@ describe("Measure with Loaction", () => {
 
     // Expand Encounter
     testPatientPage.toggleDataElement(0)
-
-    // Click cancel button for patient to proceed back to measure details
-    helper.click(testPatientPage.cancelBtn)
-
-    helper.visibleWithTimeout(measureDetailsPage.measurePageNavigationBtn)
-
-  })
+  }
 })
